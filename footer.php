d98cad3c45cae94fdffeec2a64219bb3746b68cf
Victor <victor3790@gmail.com>
2026-05-03 18:30:11 -0600

    Footer, first version

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+    if ( ! defined( 'ABSPATH' ) ) die();
+?>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p class="text-center">
+            © 2025 M. Todos los derechos reservados.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
