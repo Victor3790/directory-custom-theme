f76cb0de03bf9197e5edc4b373b39ce5b201b4fb
Victor <victor3790@gmail.com>
2026-06-28 19:15:06 -0600

    Header nav added

--- header.php
+++ header.php
@@ -17,3 +17,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php wp_head(); ?>
 </head>
 <body>
+	<header>
+		<!-- Menu -->
+		<div class="menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-6">
+						<a href="/index.html" class="m-0 menu__logo">Logo</a>
+					</div>
+					<div class="col-6">
+						<nav aria-label="Main navigation">
+							<ul class="menu__list">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="cat-archive.html">Directorio</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
