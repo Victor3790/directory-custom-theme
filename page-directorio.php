6ffbb7aea22c9c5a8563857c203a1acefc01016a
Victor <victor3790@gmail.com>
2026-06-29 11:40:03 -0600

    Directorio page added

---
+++ page-directorio.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This page will specifically show the categories (taxonomies) in the directory
+ *
+ * @package Directory
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+get_header();
+
+?>
+
+<h1>Directorio</h1>
+
+<?php
+get_footer();
