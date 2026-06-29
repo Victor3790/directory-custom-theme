0f2b190c8245f28c5c4461722b384f0f6c7105cd
Victor <victor3790@gmail.com>
2026-06-29 11:11:50 -0600

    Category archive file added

---
+++ category.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The category archive template
+ *
+ * @package Directory
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+get_header();
+
+?>
+
+<h1>Categories</h1>
+
+<?php
+get_footer();
