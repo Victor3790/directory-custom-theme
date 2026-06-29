b857c2bdbf49a1e8e3cbc4473456b5770f3abfd5
Victor <victor3790@gmail.com>
2026-06-28 19:11:30 -0600

    Header fixed

--- header.php
+++ header.php
@@ -10,14 +10,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="es-MX">
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Jardines de Morelos Ecatepec</title>
-	<link rel="icon" href="img/fav-icon.png" sizes="32x32">
-	<meta name="description" content="Descubre Jardines de Morelos, Ecatepec, un fraccionamiento con historia y tradición. Aprende cómo llegar y conoce más sobre su ubicación y características.">
-
 	<?php wp_head(); ?>
 </head>
 <body>
