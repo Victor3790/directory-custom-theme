4db8bcdcf3dd1b5df3fb58ae5e0f10bacf4f6051
Victor <victor3790@gmail.com>
2026-07-02 15:03:40 -0600

    Menu walker class added

---
+++ includes/class-directory-theme-walker-nav-menu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Custom Walker nav menu class.
+ *
+ * @package Directory
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/**
+ * Class Directory_Theme_Walker_Nav_Menu
+ */
+class Directory_Theme_Walker_Nav_Menu extends Walker_Nav_Menu {
+    
+}
